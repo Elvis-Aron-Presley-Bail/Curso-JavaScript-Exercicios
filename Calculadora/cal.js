004534bd1de5de3623233c7d5911d2237859fec4
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-03-21 11:30:05 -0300

    Teste calculadora

---
+++ Calculadora/cal.js
@@ -0,0 +1,23 @@
+let res = document.querySelector('#res')
+let priNum = ''
+let segNum = ''
+
+let num0 = document.querySelector('#num0')
+let num1 = document.querySelector('#num1')
+let num2 = document.querySelector('#num2')
+let num3 = document.querySelector('#num3')
+let num4 = document.querySelector('#num4')
+let num5 = document.querySelector('#num5')
+let num6 = document.querySelector('#num6')
+let num7 = document.querySelector('#num7')
+let num8 = document.querySelector('#num8')
+let num9 = document.querySelector('#num9')
+let mais = document.querySelector('#mais')
+let menos = document.querySelector('#menos')
+let veses = document.querySelector('#veses')
+let dividir = document.querySelector('dividir')
+let igual = document.querySelector('#igual')
+
+num0.addEventListener('click', function(){
+    res.innerHTML = 0
+})
