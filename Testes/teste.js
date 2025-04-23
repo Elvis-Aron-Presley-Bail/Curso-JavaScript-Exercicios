8db45168585384652507d66f8c56c71aa34a5d9f
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-04-23 12:35:22 -0300

    Testes

---
+++ Testes/teste.js
@@ -0,0 +1,16 @@
+let exibir = document.querySelector('#exibir')
+
+const numeros = {
+    num1 : '1',
+    num2 : '2',
+    num3 : '3',
+    num4 : '4',
+    resposta : '3',
+}
+
+function testando(nQuestao, resposta) {
+    console.log(nQuestao)
+    console.log(resposta)
+}
+
+testando('proxima')
